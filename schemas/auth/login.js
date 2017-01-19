4d581c6e0ec9566094531c83e6532c7f8cdb4eaf
Victor Huerta <vhuertahnz@gmail.com>
2017-01-18 21:35:53 -0600

    login schemas

---
+++ schemas/auth/login.js
@@ -0,0 +1,13 @@
+module.exports = {
+  type: 'object',
+  properties: {
+    email: {
+      type: 'string',
+      format: 'email'
+    },
+    password: {
+      type: 'string'
+    }
+  },
+  required: ['email', 'password']
+}
